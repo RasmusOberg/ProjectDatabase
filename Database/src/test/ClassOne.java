50c7139594797bf564cf31a7bde48b25a7b0ae51
Rasmus Oberg <rasmus.oberg@hotmail.com>
2019-10-24 15:10:39 +0200

    Update ClassOne.java

--- Database/src/test/ClassOne.java
+++ Database/src/test/ClassOne.java
@@ -4,10 +4,18 @@ import java.sql.*;
 
 public class ClassOne {
 	private Connection conn = null;
+	String SQL = "";
 
 	public static void main(String[] args) {
 		ClassOne class1 = new ClassOne();
-		class1.getId();
+//		class1.getAllBands("bandname");
+//		class1.showAct();
+//		class1.getTime("Stora scen", "Teddybears");
+//		class1.getAllActs("Stora scen");
+//		class1.getBand("Stora scen", "16:00");
+//		class1.getStage("Teddybears", "16:00", "Torsdag");
+//		class1.getBandOnStage("16:00", "Torsdag");
+		class1.getStageAndTime("Teddybears");
 	}
 
 	public Connection connect() {
@@ -17,15 +25,13 @@ public class ClassOne {
 			e.printStackTrace();
 		}
 
-		String url = "jdbc:postgresql://localhost/postgres";
+		String url = "jdbc:postgresql://localhost/mortfors";
 		String user = "rasmusoberg";
 		String password = "fzLtBWF3Qxb2juq";
 		
 //		String url = "jdbc:postgresql://pgserver.mah.se:80/AE7610";
 //		String user ="AE7610";
 //		String password ="tnoogmwp";
-		
-		
 		
 		try {
 			conn = DriverManager.getConnection(url, user, password);
@@ -36,39 +42,146 @@ public class ClassOne {
 		
 		return conn;
 	}
+	
 
-	public void getId() {
-		String SQL = "SELECT * FROM Employee";
+	public void getTime(String stagename, String bandname) {
+		String SQL = "SELECT act.start, act.endtime, act.date\n" + 
+				"FROM act\n" + 
+				"WHERE bandid =\n" + 
+				"      (SELECT bands.id FROM bands WHERE bandname = '" + bandname + "')\n" + 
+				"  AND stageid =\n" + 
+				"      (SELECT stages.id FROM stages WHERE stages.name = '" + stagename + "');";
+		
+		try(Connection conn1 = connect();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL)){
+			while(rs.next()) {
+				System.out.println(rs.getString("start") + rs.getString("endtime") + rs.getString("date"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void getAllActs(String stagename) {
+		String SQL = "SELECT bands.bandname, act.start, act.endtime, act.date\n" + 
+				"      FROM act\n" + 
+				"        INNER JOIN bands\n" + 
+				"         ON act.bandid = bands.id\n" + 
+				"      WHERE stageid =\n" + 
+				"                    (SELECT stages.id\n" + 
+				"                    FROM stages\n" + 
+				"                    WHERE stages.name = '" + stagename + "');";
+		try(Connection conn1 = connect();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL)){
+			while(rs.next()) {
+				System.out.println(rs.getString("bandname") + rs.getString("start") +
+						rs.getString("endtime") + rs.getString("date"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void getStage(String band, String starttime, String day) {
+		String SQL = "SELECT stages.name\n" + 
+				"FROM stages\n" + 
+				"INNER JOIN act ON stages.id = act.stageid\n" + 
+				"    WHERE act.bandid = (SELECT bands.id FROM bands WHERE bands.bandname = '" + band + "')\n" + 
+				"        AND act.start = '" + starttime + "'\n" + 
+				"        AND act.date = '" + day + "';";
+		try(Connection conn1 = connect();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL)){
+			while(rs.next()) {
+				System.out.println(rs.getString("name"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void getBandOnStage(String starttime, String day) {
+		String SQL = "SELECT bands.bandname, stages.name\n" + 
+				"    FROM act\n" + 
+				"        INNER JOIN stages ON act.stageid = stages.id\n" + 
+				"        INNER JOIN bands ON act.bandid = bands.id\n" + 
+				"    WHERE act.start = '" + starttime + "'\n" + 
+				"            AND act.date = '" + day + "';";
+		try(Connection conn1 = connect();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL)){
+			while(rs.next()) {
+				System.out.println(rs.getString("bandname") + rs.getString("name"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void getBand(String stagename, String starttime) {
+		String SQL = "SELECT bands.bandname\n" + 
+				"from bands\n" + 
+				"INNER JOIN act ON bands.id = act.bandid\n" + 
+				"    where act.stageid = (SELECT stages.id FROM stages WHERE stages.name = '" + stagename + "')\n" + 
+				"        AND act.start = '16:00'\n" + 
+				"        AND act.date = 'Torsdag';";
+		
+		try(Connection conn1 = connect();
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL)){
+			while(rs.next()) {
+				System.out.println(rs.getString("bandname"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void getAllBands(String str) {
+		String SQL = "SELECT " + str + " FROM bands";
 		System.out.println("1");
 
 		try (Connection conn1 = connect();
 				Statement stmt = conn.createStatement();
 				ResultSet rs = stmt.executeQuery(SQL)) {
-			displayActor(rs);
+			displayBands(rs, str);
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
 	}
-
-	private void displayActor(ResultSet rs) throws SQLException {
-		while (rs.next()) {
-			System.out.println(rs.getString("first_name"));
-		}
-	}
-
-	public int getActorCounts() {
-		String SQL = "SELECT count(*) FROM Employee";
-		int count = 0;
-
-		try (Connection conn1 = connect();
+	
+	public void getStageAndTime(String bandname) {
+		String SQL = "SELECT stages.name, act.start, act.endtime, act.date\n" + 
+				"    FROM act\n" + 
+				"        INNER JOIN stages ON act.stageid = stages.id\n" + 
+				"    WHERE act.bandid = (\n" + 
+				"                        SELECT bands.id\n" + 
+				"                        FROM bands\n" + 
+				"                        WHERE bands.bandname = '" + bandname + "');";
+		try(Connection conn1 = connect();
 				Statement stmt = conn.createStatement();
-				ResultSet rs = stmt.executeQuery(SQL)) {
-			rs.next();
-			count = rs.getInt(1);
-		} catch (SQLException ex) {
-			ex.printStackTrace();
+				ResultSet rs = stmt.executeQuery(SQL)){
+			while(rs.next()) {
+				System.out.println(rs.getString("name") + rs.getString("start") + rs.getString("endtime") + rs.getString("date"));
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
 		}
-		return count;
+	}
+
+	private void displayBands(ResultSet rs, String str) throws SQLException {
+		while (rs.next()) {
+			System.out.println(rs.getString(str));
+		}
+	}
+	
+	private void displayBand(ResultSet rs, String str) throws SQLException {
+		while (rs.next()) {
+			System.out.println(rs.getString(str));
+		}
 	}
 
 }
